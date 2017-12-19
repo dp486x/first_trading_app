04a06c1a4ca0e4a0ac28a16ad4c9988c7c100770
Dinesh kumar <Poladinesh2000@yahoo.com>
2017-12-19 17:32:32 -0600

    initial server.js

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const chalk = require('chalk');
+const morgan = require('morgan');
+const route = express.Router();
+const port = 3333;
+
+//Static files path
+app.use(express.static('__dirName'));
+
+app.use(morgan('dev', {
+  skip: function(req, res) {
+    return res.statusCode < 400
+  },
+  stream: process.stderr
+}));
+
+app.use(morgan('dev', {
+  skip: function(req, res) {
+    return res.statusCode >= 400
+  },
+  stream: process.stdout
+}));
+
+app.use('/user', require('./app/controllers/user_controller'));
+
+app.get('/', (req, res) => {
+  res.send("you deserve a fruit");
+});
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log("Server started at : " + chalk.blue(port));
+})
